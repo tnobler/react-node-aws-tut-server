2d92c97dd6ef453064693eb78f25b40243aa0efb
Tanner Noble <tannernoble@gmail.com>
2020-07-06 17:17:07 -0500

    Initialize repo, setup server with nodemon and express, and wired up an auth routes.

---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/register", (req, res) => {
+  res.json({
+    data: "you hit register endpoint middleware...",
+  });
+});
+
+module.exports = router;
